598ea8cd7d44c44158561b5ec1179f3f939a42b1
Kwatra <vishal.kwatra@sap.com>
2022-09-22 19:00:57 +0530

    Odata Servlet in web.xml via ApacheOlingo ODataJPAServiceFactory

---
+++ src/main/java/servlets/ODataServiceFactory.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.olingo.odata2.jpa.processor.api.ODataJPAContext;
+import org.apache.olingo.odata2.jpa.processor.api.ODataJPAServiceFactory;
+import org.apache.olingo.odata2.jpa.processor.api.exception.ODataJPARuntimeException;
+
+public class ODataServiceFactory extends ODataJPAServiceFactory {
+
+	private static final String PUNIT_NAME = "odatatest";
+	private DataSource ds;
+	private EntityManagerFactory emf;
+
+	@Override
+	public ODataJPAContext initializeODataJPAContext() throws ODataJPARuntimeException {
+		try {
+			InitialContext ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/DefaultDB");
+//
+//            Map properties = new HashMap();
+//            properties.put(PersistenceUnitProperties.NON_JTA_DATASOURCE, ds);
+//            emf = Persistence.createEntityManagerFactory("odatatest", properties);
+			emf = Persistence.createEntityManagerFactory(PUNIT_NAME);
+		} catch (NamingException e) {
+			try {
+				throw new ServletException(e);
+			} catch (ServletException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		ODataJPAContext oDataJpaCtx = getODataJPAContext();
+		oDataJpaCtx.setEntityManagerFactory(emf);
+		oDataJpaCtx.setPersistenceUnitName(PUNIT_NAME);
+		// TODO Auto-generated method stub
+		return oDataJpaCtx;
+	}
+
+}
